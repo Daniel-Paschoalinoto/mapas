a8559298bd0865f69ba65c4dc8a5a5920c1ef34b
Daniel Paschoalinoto <daniel.sp@empregos.com.br>
2024-05-11 17:01:49 -0300

    Refactor, agora 100% funcional e escalável

--- scripts.js
+++ scripts.js
@@ -1,65 +1,41 @@
-function copiarXpath1(btn, event) {
-    // Encontra o elemento pai do botão clicado
-    var parentDiv = btn.parentNode;
+const processadorBanco = "(.+Banco de currículos.+)|(.+banco de curriculos.+)|(.+baco de currículo.+)|(.+Banco de curriculo.+)|(.+banco de talentos.+)|(.+banco de talento.+)|(.+banco de Candidatos.+)|(.+banco de candidato.+)|(.+Banco de currículos)|(.+banco de curriculos)|(.+baco de currículo)|(.+Banco de curriculo)|(.+banco de talentos)|(.+banco de talento)|(.+banco de Candidatos)|(.+banco de candidato)|(Banco de currículos.+)|(banco de curriculos.+)|(baco de currículo.+)|(Banco de curriculo.+)|(banco de talentos.+)|(banco de talento.+)|(banco de Candidatos.+)|(banco de candidato.+)|(.+Banco de Currículos.+)|(.+VAGA BANCO.+)"
+const descricaoPadrao = "Atuará nas atividades internas e demais funções pertinentes ao cargo. Necessário conhecimento na área de atuação."
 
-    // Seleciona o texto dentro do span dentro do elemento pai
-    var xpathExpression1 = parentDiv.querySelector('.xpath-expression1').innerText;
-
-    // Copia o texto para a área de transferência
-    navigator.clipboard.writeText(xpathExpression1).then(function() {
-        // Mostrar mensagem próxima ao cursor do mouse
-        showTooltip('Copiado', event);
-    }).catch(function(err) {
-        // Caso ocorra algum erro
-        console.error('Erro ao copiar XPath: ', err);
-    });
+async function copiarElementoAnterior(button) {
+    // Obtém o elemento anterior ao botão
+    var elementoAnterior = button.previousElementSibling;
+    var texto = elementoAnterior.innerText;
+    await navigator.clipboard.writeText(texto);
+    exibirNotificacao(button, 'Copiado');
 }
 
-function copiarProcessador1(btn, event) {
-    // Encontra o elemento pai do botão clicado
-    var parentDiv = btn.parentNode;
-
-    // Seleciona o texto dentro do span dentro do elemento pai
-    var regexExpression1 = parentDiv.querySelector('.regex-expression1').innerText;
-
-    // Copia o texto para a área de transferência
-    navigator.clipboard.writeText(regexExpression1).then(function() {
-        // Mostrar mensagem próxima ao cursor do mouse
-        showTooltip('Copiado', event);
-    }).catch(function(err) {
-        // Caso ocorra algum erro
-        console.error('Erro ao copiar Regex: ', err);
-    });
+async function copiarTextoDoBotao(button) {
+    // Obtém o texto do botão
+    var texto = button.innerText;
+    await navigator.clipboard.writeText(texto);
+    exibirNotificacao(button, 'Copiado');
 }
 
-function copiarProcessador2(btn, event) {
-    // Encontra o elemento pai do botão clicado
-    var parentDiv = btn.parentNode;
-
-    // Seleciona o texto dentro do span dentro do elemento pai
-    var regexExpression2 = parentDiv.querySelector('.regex-expression2').innerText;
-
-    // Copia o texto para a área de transferência
-    navigator.clipboard.writeText(regexExpression2).then(function() {
-        // Mostrar mensagem próxima ao cursor do mouse
-        showTooltip('Copiado', event);
-    }).catch(function(err) {
-        // Caso ocorra algum erro
-        console.error('Erro ao copiar Regex: ', err);
-    });
+async function copiarConteudoProcessador(button, processador) {
+    await navigator.clipboard.writeText(processador);
+    exibirNotificacao(button, 'Copiado');
 }
 
-// Função para mostrar a caixa de mensagem
-function showTooltip(message, event) {
-    var tooltip = document.getElementById("tooltip");
-    tooltip.innerText = message;
-    tooltip.style.display = "block";
-    tooltip.style.top = (event.clientY + 10) + "px";
-    tooltip.style.left = (event.clientX + 10) + "px";
-  
-    // Oculta a caixa de mensagem após alguns segundos
-    setTimeout(function() {
-      tooltip.style.display = "none";
-    }, 600);
-  }
-  
+function exibirNotificacao(button, mensagem) {
+    var tooltip = document.querySelector('.tooltip');
+    tooltip.innerText = mensagem;
+    tooltip.style.display = 'block';
+
+    var rect = button.getBoundingClientRect();
+    var x = rect.left + rect.width; // Ajuste para a direita
+    var y = rect.top - 20; // Ajuste para cima
+
+    // Define a posição da notificação próxima ao botão
+    tooltip.style.top = y + 'px';
+    tooltip.style.left = x + 'px';
+
+    // Oculta a notificação após um período de tempo
+    setTimeout(function () {
+        tooltip.style.display = 'none';
+    }, 500); // Altere o tempo conforme necessário
+}
